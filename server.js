d587b72bedeb45bc44d1ee9aa5df4e28bb8790ab
Albert Czarnecki <abath@vp.pl>
2018-06-06 10:08:08 +0200

    saved server.js file xD

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,10 +18,23 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/timestamp/", function (req, res) {
+  var date = new Date(Date.now());
+  res.json({ "unix": date.getTime(), "utc": date.toUTCString() })
+});
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string", function (req, res) {
+  if (req.params.date_string.includes("-")) {
+    var date = new Date(req.params.date_string);
+  } else {
+    var date = new Date(parseInt(req.params.date_string));
+  }
+
+  if (isNaN(date.getTime())) {
+    res.json({ "unix": "null", "utc": "Invalid date" });
+  } else {
+    res.json({ "unix": date.getTime(), "utc": date.toUTCString() })
+  }
 });
 
 
